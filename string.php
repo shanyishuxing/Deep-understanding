93c861d9da101649f0ce5d51f5eca1fbe460a457
wuyufan888 <152785469@qq.com>
2019-12-19 17:04:15 +0800

    add 约瑟夫问题和字符串反转

---
+++ string.php
@@ -0,0 +1,27 @@
+﻿<?php
+//string统计字符串出行的次数
+
+//one.自定义函数 可以反转数字英文，反转中文乱码
+header("content-type:text/html;chartset=utf-8");
+
+$str = 'AbCdEfGaBcDeFgH0234;,!-AaBbCcDdEeFfGg';
+
+$str = strtoupper($str);          // 不区分大小写时,全部转换成大写或者小写
+
+// 方法一
+$res = array();                   // 定义一个结果集空数组
+$arr = str_split($str);           // 将字符串转换成数组
+$res = array_count_values($arr);  // 统计数组中各个值出现的次数
+var_dump($res);
+
+$str=' yauiopajaql';
+echo substr_count($str,'a');
+
+
+$str1 = 'yabadabadoo';
+$str2 = 'yaba';
+if (strpos($str1,$str2)) {  
+    echo "\"" . $str1 . "\" contains \"" . $str2 . "\"";
+} else {
+    echo "\"" . $str1 . "\" does not contain \"" . $str2 . "\"";
+}
---
+++ ysf.php
@@ -0,0 +1,76 @@
+﻿<?php
+//约瑟夫环问题
+
+function monkeyKing($n,$m)
+{
+	$arr=range(1,$n);
+	$i=1;
+	while(count($arr)>1){//只留一个活口
+		if($i%$m<>0){
+			$arr[]=$arr[$i-1];
+		}
+		unset($arr[$i-1]);
+		$i++;
+	}
+	//$arr[0]=$i;
+	return $arr[$i-1];
+}
+//echo "猴子大王是".monkeyKing(1000000,3);
+
+//留存活口数量约瑟夫环
+function monkeyKing1($n,$m,$s)
+{
+	$arr=range(1,$n);
+	$i=1;
+	while(count($arr)>$s){
+		if($i%$m<>0){
+			$arr[]=$arr[$i-1];
+		}
+		unset($arr[$i-1]);
+		$i++;
+	}
+	//$arr[0]=$i;
+	return $arr;
+}
+echo "猴子大王是";
+//var_dump(monkeyKing1(1000000,3,2));
+
+
+
+//约瑟夫环，栈理解
+function ysf($n,$m,$s)
+{
+	$arr=range(1,$n);
+	$i=0;
+	while(count($arr)>$s){ //保留几个人留几个人
+		++$i;
+		$survive=array_shift($arr);
+		if($i%$m!=0){
+			array_push($arr,$survive);			
+		}
+	}
+	return $arr;
+}
+
+echo "猴子大王是";var_dump(ysf(1000000,3,2));
+
+function yuesefu($n,$m)
+{
+	$r=0;
+	for($i=2;$i<=$n;$i++){
+		$r=($r+$m)%$i;
+	}
+	return $r+1;		
+}
+
+echo "<br/>猴子大王是".yuesefu(1000000,3);
+
+//数组随机取得
+function arr_rand($n,$m){
+	$arr=array_rand(range(1,10000),100);
+	var_dump($arr);
+}
+
+
+
+
